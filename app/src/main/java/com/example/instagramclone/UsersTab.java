07e8437a29e0f7988f5a0cd6316b8309cb006951
oszkarpap <scaevola.c@gmail.com>
2019-04-10 10:55:03 +0200

    Users post

--- app/src/main/java/com/example/instagramclone/UsersTab.java
+++ app/src/main/java/com/example/instagramclone/UsersTab.java
@@ -1,34 +1,38 @@
 package com.example.instagramclone;
 
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
-
-import org.w3c.dom.Text;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class UsersTab extends Fragment {
+
+public class UsersTab extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
+
 
     private ListView listView;
-    private ArrayList arrayList;
+    private ArrayList<String> arrayList;
     private ArrayAdapter arrayAdapter;
 
     public UsersTab() {
@@ -46,8 +50,10 @@ public class UsersTab extends Fragment {
         arrayList = new ArrayList();
         arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, arrayList);
 
-        final TextView txtLoadingData = view.findViewById(R.id.txtLoadingUsers);
+        listView.setOnItemClickListener(UsersTab.this);
+        listView.setOnItemLongClickListener(UsersTab.this);
 
+        final TextView txtLoadingUsers = view.findViewById(R.id.txtLoadingUsers);
         ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
 
         parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
@@ -56,20 +62,81 @@ public class UsersTab extends Fragment {
             @Override
             public void done(List<ParseUser> users, ParseException e) {
                 if (e == null) {
+
                     if (users.size() > 0) {
+
                         for (ParseUser user : users) {
+
                             arrayList.add(user.getUsername());
+
                         }
 
                         listView.setAdapter(arrayAdapter);
-                        txtLoadingData.animate().alpha(0).setDuration(1000);
+                        txtLoadingUsers.animate().alpha(0).setDuration(2000);
                         listView.setVisibility(View.VISIBLE);
+
                     }
+
+                }
+            }
+        });
+        return view;
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Intent intent = new Intent(getContext(), usersPosts.class);
+        intent.putExtra("username", arrayList.get(position));
+        startActivity(intent);
+
+    }
+
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereEqualTo("username", arrayList.get(position));
+        parseQuery.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+
+                if (user != null && e == null) {
+
+//                    FancyToast.makeText(getContext(), user.get("profileProfession") + "",
+//                            Toast.LENGTH_SHORT, FancyToast.SUCCESS,
+//                            true).show();
+                    final PrettyDialog prettyDialog =  new PrettyDialog(getContext());
+
+                    prettyDialog.setTitle(user.getUsername() + " 's Info")
+                            .setMessage(user.get("profileBio") + "\n"
+                                    + user.get("profileProfession") + "\n"
+                                    + user.get("profileHobbies") + "\n"
+                                    + user.get("profileFavSport"))
+                            .setIcon(R.drawable.ic_contact_mail_black_24dp)
+                            .addButton(
+                                    "OK",     // button text
+                                    R.color.pdlg_color_white,  // button text color
+                                    R.color.pdlg_color_green,  // button background color
+                                    new PrettyDialogCallback() {  // button OnClick listener
+                                        @Override
+                                        public void onClick() {
+                                            // Do what you gotta do
+                                            prettyDialog.dismiss();
+                                        }
+                                    }
+                            )
+                            .show();
+
+
                 }
             }
         });
 
-        return view;
-    }
 
+        return true;
+    }
 }
